8d2fc0ec0bdce828f464ea3e4d54f78f0743bb77
Carlos Alexandre Cavalheiro <58004969+carlosNineBit@users.noreply.github.com>
2022-03-21 17:19:23 -0300

    Update Condutores.php

--- Condutores.php
+++ Condutores.php
@@ -13,10 +13,10 @@ Class Condutores
 	{
 		// ----------------------- MYSQL --------------------
 		// conexão com a base de dados MySql 
-		$host = "localhost";
-		$usuario = "??????";
-		$senha = "????????";
-		$bd = "??????";
+		$host = "sql10.freesqldatabase.com";
+		$usuario = "8UMyvqgh42";
+		$senha = "sql10480617";
+		$bd = "sql10480617";
 		$this->con = mysqli_connect($host, $usuario, $senha, $bd);
 		return $this->con;
 	}
